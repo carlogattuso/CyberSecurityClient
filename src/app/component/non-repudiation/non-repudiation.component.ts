67ecdd822e93b191757c1d389360020d49e549ff
SergiVera <sergitus1998@gmail.com>
2020-04-04 20:23:23 +0200

    trim Digest added

--- src/app/component/non-repudiation/non-repudiation.component.ts
+++ src/app/component/non-repudiation/non-repudiation.component.ts
@@ -204,7 +204,7 @@ export class NonRepudiationComponent implements OnInit {
         this.bPubKey = new rsa.PublicKey(bc.hexToBigint(res.pubKey.e), bc.hexToBigint(res.pubKey.n));
         let proofDigest = bc.bigintToHex(await this.bPubKey.verify(bc.hexToBigint(res.signature)));
         let bodyDigest = await sha.digest(res.body);
-        if (bodyDigest === proofDigest) {
+        if (bodyDigest.trim() === proofDigest.trim()) {
           this.pr = res.signature;
           let body = JSON.parse(JSON.stringify({type: 3, src: 'A', dst: 'TTP', msg: this.key, timestamp: Date.now()}));
 
