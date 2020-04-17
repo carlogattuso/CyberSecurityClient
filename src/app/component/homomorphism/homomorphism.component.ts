50f25f3ca0db572129e2bcf280241bb87d003ad2
Andreagalera <1d09galeraandrea@gmail.com>
2020-04-17 12:56:10 +0200

    Added polling

--- src/app/component/homomorphism/homomorphism.component.ts
+++ src/app/component/homomorphism/homomorphism.component.ts
@@ -3,6 +3,8 @@ import * as paillier from 'paillier-bigint';
 import { NgForm } from '@angular/forms';
 import { HomomorphismService } from '../../services/homomorphism.service';
 import * as bc from 'bigint-conversion';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-homomorphism',
@@ -19,8 +21,44 @@ export class HomomorphismComponent implements OnInit {
 
   keyPairPallier;
 
+  /**
+   * Counter of votes for each contestant
+   */
+  votesA = 0;
+  votesB = 0;
+  votesC = 0;
 
-  constructor(public clientService: HomomorphismService) { }
+  /**
+   * List of encrypted votes
+   */
+  listAllVotes: Array<bigint> = [];
+
+  /**
+   * To know if the button is clicked in order to show the results
+   */
+  clicked = false;
+
+  /**
+   * The final result of the encrypted votes
+   */
+  totalDecrypted = BigInt(1);
+
+
+  /**
+   * Homomorphic poll Form to manage messages
+   * @name homomorphicForm
+   * @type {FormGroup}
+   */
+  pollForm: FormGroup;
+
+  /**
+   *
+   * @param clientService
+   * @param formBuilder
+   */
+  constructor(public clientService: HomomorphismService, private formBuilder: FormBuilder) {
+    this.pollForm = this.formBuilder.group({});
+  }
 
   ngOnInit(): void {
     this.getPublicKeyPailler();
@@ -29,7 +67,7 @@ export class HomomorphismComponent implements OnInit {
   // Función para obtener la public key de B
   async getPublicKeyPailler() {
 
-    this.keyPairPallier = await paillier.generateRandomKeys(3072);
+    this.keyPairPallier = await paillier.generateRandomKeys(512);
 
     this.publicKey = new paillier.PublicKey(
       this.keyPairPallier["publicKey"]["n"],
@@ -38,10 +76,10 @@ export class HomomorphismComponent implements OnInit {
 
     this.privateKey = new paillier.PrivateKey(
       this.keyPairPallier["privateKey"]["lambda"],
-      this.keyPairPallier["privateKey"]["mu"],
-      this.keyPairPallier["privateKey"]["publicKey"],
-      this.keyPairPallier["privateKey"]["_p"],
-      this.keyPairPallier["privateKey"]["_q"]
+      this.keyPairPallier["privateKey"]['mu'],
+      this.keyPairPallier["privateKey"]['publicKey'],
+      this.keyPairPallier["privateKey"]['_p'],
+      this.keyPairPallier["privateKey"]['_q']
     );
 
     // this.clientService.getPublicKeyPailler().subscribe(data => {
@@ -58,10 +96,42 @@ export class HomomorphismComponent implements OnInit {
     console.log(this.privateKey);
   }
 
+  async voteA() {
+    this.votesA++;
+    const A = BigInt(100);
+    const encryptedA = await this.publicKey.encrypt(A);
+    this.listAllVotes.push(encryptedA);
+    console.log(this.listAllVotes);
+  }
 
+  async voteB() {
+    this.votesB++;
+    const B = BigInt(10);
+    const encryptedB = await this.publicKey.encrypt(B);
+    this.listAllVotes.push(encryptedB);
+    console.log(this.listAllVotes);
+  }
+
+  async voteC() {
+    this.votesC++;
+    const C = BigInt(1);
+    const encryptedC = await this.publicKey.encrypt(C);
+    this.listAllVotes.push(encryptedC);
+    console.log(this.listAllVotes);
+  }
+
+  async recountVotes() {
+    this.clicked = true;
+    let totalEncrypted = BigInt(1);
+    for (let i in this.listAllVotes) {
+      totalEncrypted = this.publicKey.addition(this.listAllVotes[i], totalEncrypted);
+    }
+    console.log(this.privateKey.decrypt(totalEncrypted));
+    this.totalDecrypted = this.privateKey.decrypt(totalEncrypted);
+    document.getElementById('recountVotes').style.display = 'none';
+  }
 
   async sendWords(form: NgForm) {
-    console.log("Hola");
     const m1 = bc.textToBigint(form.value.wordOne);
     const m2 = bc.textToBigint(form.value.wordTwo);
     console.log(m1);
